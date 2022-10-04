35d721feac1ff5baa09b8eb49187b792c6915d8d
KreitinnSoftware <80591934+KreitinnSoftware@users.noreply.github.com>
2022-10-04 17:34:13 -0300

    Dialog in Extraction and Flash

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-echo Verificando Dependências...
+echo Checking Dependencies...
 sleep 1
 if [ ! -e /usr/bin/yad ]; then
     dps=$(($dps+1))
@@ -7,22 +7,22 @@ if [ ! -e /usr/bin/heimdall ]; then
     dps=$(($dps+1)) 
 fi
 case $dps in 2)
-    echo Instalando Dependências...
+    echo Installing Dependencies...
     sudo apt install heimdall-flash yad -y
 esac
 case $dps in 1)
-    echo Instalando Dependências...
-    sudo apt install yad -y
+    echo Installing Dependencies...
+    sudo apt install heimdall-flash yad -y
 esac
-echo Instalando SamFlasher Tool v1.0...
+echo Installing SamFlasher Tool v1.2...
 sleep 1
 sudo cp samflasher-tool /usr/bin/samflasher-tool
 sudo chmod +x /usr/bin/samflasher-tool
 cp icon.png $HOME/.local/share/icons/samflashertool.png    
 sm=SamFlasherTool.desktop
 echo [Desktop Entry] > $sm
-echo Name=SamFlasher Tool v1.0 >> $sm
-echo Comment=SamFlasher Tool based on Heimdall by Kreitinn >> $sm
+echo Name=SamFlasher Tool v1.2 >> $sm
+echo Comment=SamFlasher Tool based on Heimdall >> $sm
 echo Exec=/usr/bin/samflasher-tool >> $sm
 echo Icon=$HOME/.local/share/icons/samflashertool.png >> $sm
 echo Terminal=false >> $sm
@@ -30,4 +30,4 @@ echo StartupWMClass=SamFlasherTool >> $sm
 echo Type=Application >> $sm
 echo 'Categories=Other;' >> $sm
 mv $sm $HOME/.local/share/applications/$sm
-echo SamFlasher Tool v1.0 instalado com sucesso.
+echo SamFlasher Tool v1.2 installed sucessfully.
